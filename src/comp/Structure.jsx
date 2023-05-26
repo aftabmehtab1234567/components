8e21d9d21dba45ca32281522d11179d6879f4a66
Aftab Alam <aftabalam9008@gmail.com>
2023-05-26 17:15:38 +0530

    push code

--- src/comp/Structure.jsx
+++ src/comp/Structure.jsx
@@ -5,8 +5,17 @@ const Structure = (props) => {
   console.log(props);
   return (
     <>
+    <div className="card" style={{width: "18rem"}}>
+      <div className="row">
     <img src= {props.img} />
-    <h1> {props.name}</h1>
+  <div className="card-body">
+  <h1>{props.name}</h1>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+    
+  
     </>
   )
 }
